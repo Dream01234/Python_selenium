5c3fda1bc34bdbb2b3b03e1f92f288a714d0f83e
Dr.Li <417081473@qq.com>
2023-11-23 16:50:15 +0800

    添加手机浏览器模式

--- main.py
+++ main.py
@@ -31,9 +31,13 @@ token1 = jsonpath.jsonpath(json_data,token)
 token2 = "".join(token1)
 print(token1)
 
+# 设置手机模拟器模式
+options = webdriver.ChromeOptions()
+options.add_experimental_option('mobileEmulation',{'deviceName':'iPhone X'})
 
 # 创建浏览器驱动对象
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
+
 
 url2 = 'https://devtest3.buildingqm.com/public/app3/bpm/app/nav.html?lang=zh_CN&token='+token2+'&project_id=100496&team_id=100338&group_id=100337&en_name=bsi_rfi&page_level=project&show_other=1'
 # # 打开百度首页
@@ -43,11 +47,10 @@ sleep(5)
 driver.find_element(by=By.XPATH,value="/html/body/div[1]/div[1]/ul/li/div").click()
 
 # 浏览器窗口最大化
-driver.maximize_window()
+# driver.maximize_window()
 
 # 休眠2s
 sleep(30)
-
 
 # 关闭窗口
 driver.quit()
