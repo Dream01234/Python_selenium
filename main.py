0ca30db84f2be04880f211d9855f97a03eaa3b0b
Dr.Li <417081473@qq.com>
2023-11-23 16:32:36 +0800

    添加备注

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import json
-
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 import requests
 import jsonpath
 
+# 请求头
 Headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
          }
-
+# 测试地址
 url = 'https://devtest3.buildingqm.com/uc/app/verify_login/'
+
+# 测试数据
 data = {
     'device_id':'351564297070492',
     'username':'kentestgrp10',
     'password':'12345678'
 }
+
+# 发送请求
 r = requests.post(url,data=data,headers=Headers)
 json_data = r.json()
 print(json_data)
 
-
+# 提取token
 token = '$.data.token'
 token1 = jsonpath.jsonpath(json_data,token)
+
+# 将list类型转换为str
 token2 = "".join(token1)
 print(token1)
 
@@ -44,4 +50,4 @@ sleep(30)
 
 
 # 关闭窗口
-# driver.quit()
+driver.quit()
